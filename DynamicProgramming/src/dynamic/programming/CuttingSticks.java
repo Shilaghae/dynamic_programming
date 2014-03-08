29cf55fcb2c3f0ffcb6253afab6eac9864871cf0
annadelprete <delprete.ann@gmail.com>
2014-03-08 11:06:15 +0000

    Exercises

---
+++ DynamicProgramming/src/dynamic/programming/CuttingSticks.java
@@ -0,0 +1,26 @@
+package dynamic.programming;
+
+public class CuttingSticks {
+
+	int[] cuttingSticks(int[] p, int n){
+		
+		int[] r,s;
+		r = new int[n+1];
+	    s = new int[n+1];
+	    r[0] = 0;
+	    s[0] = 0;
+	    int q;
+		for(int j=1;j<=n;j++){ 
+			q= Integer.MIN_VALUE;
+			for(int i=1;i<=j;i++){ 
+				if(q<p[i]+r[j-i]){
+					q=p[i]+r[j-i];
+					s[j] = i;
+				}
+			}
+			r[j] = q;
+		}
+		return s;
+	}
+
+}
