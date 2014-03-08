4903836940d57098d227544a13ce50177f281e62
annadelprete <delprete.ann@gmail.com>
2014-03-08 11:10:57 +0000

    .

---
+++ DynamicProgramming/src/dynamic/programming/CoinChangeTest.java
@@ -0,0 +1,38 @@
+package dynamic.programming;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CoinChangeTest {
+
+	private CoinChange coinChange;
+
+	@Before
+	public void setUp() throws Exception {
+		coinChange = new CoinChange();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test1() {
+		int f = coinChange.findCombinationsCount(11, new int[]{1,5,10,25,50});
+		assertEquals("Wrong combination!", 4, f);
+	}
+	@Test
+	public void test2() {
+		int f = coinChange.findCombinationsCount(26, new int[]{1,5,10,25,50});
+		assertEquals("Wrong combination!", 13, f);
+	}
+	@Test
+	public void test3() {
+		int f = coinChange.findCombinationsCount(7489, new int[]{1,5,10,25,50});
+		assertEquals("Wrong combination!", 2146113925, f);
+	}
+
+}
